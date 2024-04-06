3279ba6946734d7fcd9b63e9bca9d73c089a4820
stevo <spolin44@gmail.com>
2024-04-06 19:28:18 -0400

    added ParseAnyArr

--- any.go
+++ any.go
@@ -6,3 +6,13 @@ func AnyArr[T any](arr []T) (res []any) {
 	}
 	return
 }
+
+func ParseAnyArr[T any](arr []any) (res []T) {
+	for _, v := range arr {
+		switch v := v.(type) {
+		case T:
+			res = append(res, v)
+		}
+	}
+	return
+}
