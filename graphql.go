6d7b2c3d71346beefa50d6e006c2d5a9b9edce18
stevo <spolin44@gmail.com>
2024-05-04 22:40:19 -0400

    made graphql have omit empty

--- graphql.go
+++ graphql.go
@@ -3,9 +3,9 @@ package structures
 import "encoding/json"
 
 type GraphqlPayload struct {
-	OperationName string         `json:"operationName"`
-	Query         string         `json:"query"`
-	Variables     map[string]any `json:"variables"`
+	OperationName string         `json:"operationName,omitempty"`
+	Query         string         `json:"query,omitempty"`
+	Variables     map[string]any `json:"variables,omitempty"`
 }
 
 func NewGraphqlPayload(operationName string, query string, variables map[string]any) *GraphqlPayload {
